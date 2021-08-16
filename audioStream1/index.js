4cf8100d7fd2c7b647219074f3f8aecb2e4882b8
Abdulrahman Salau <bob.salau@gmail.com>
2021-08-16 21:33:55 +0100

    async code refactor

--- audioStream1/index.js
+++ audioStream1/index.js
@@ -1,95 +1,13 @@
-const fs = require(`fs`);
-const Throttle = require(`throttle`);
-const path = require(`path`);
-const filename = `12-de_la_soul_feat_little_dragon-drawn-ade57ca6.mp3`;
-const filePath = path.join(__dirname, `music`, filename);
+`use strict`;
 const http = require(`http`);
+const server = http.createServer();
+const {serveStream} = require(`./controller`);
 
-var server = http.createServer(function(req, res) {
-    const readable = fs.createReadStream(filePath);
-    const stat = fs.statSync(filePath);
-    const rangeRequest = readRangeHeader(req.headers[`range`], stat.size);
-    // const throttle = new Throttle(320000 / 8);
-    //const t = readable.pipe(throttle);
- 
-    // If 'Range' header exists, we will parse it with Regular Expression.
-    if (rangeRequest == null) {
-        res.setHeader(`Content-Type`, `audio/mp3`);
-        res.setHeader(`Content-Length`, stat.size);  // File size.
-        res.setHeader(`Accept-Ranges`, `bytes`);
+server.on('request',catchErrors(serveStream))
 
-        readable.pipe(res);
-        return null;
-    }
-
-    const start = rangeRequest.Start;
-    const end = rangeRequest.End;
-    // If the range can't be fulfilled.
-    if (start >= stat.size || end >= stat.size) {
-        // Indicate the acceptable range.
-        res.setHeader(`Content-Range`, `bytes */${stat.size}`);
-
-        // Return the 416 'Requested Range Not Satisfiable'.
-        res.writeHead(416);
-        return null;
-    }
-
-    res.setHeader(`Content-Type`, `audio/mp3`);
-    res.setHeader(`Content-Length`, stat.size);
-    res.setHeader(`Content-Range`, `bytes ${start}-${end}/${stat.size}`);
-    res.setHeader(`Accept-Ranges`, `bytes`);
-    res.setHeader(`Cache-Control`, `no-cache`);
-    readable.pipe(res);
-
-    // t.on('data', (chunk)=>{
-    //     res.write(chunk);
-    //     res.writeHead(206, responseHeaders);
-    // });
-
-    // t.on('error', (err) => {
-    //     console.error(err);
-    //     res.end();
-    //   });
-
-    // t.on('end', () => {
-    //     res.end();
-    //   });
-
-});
-
-
-function readRangeHeader(range, totalLength) {
-    /*
-     * Example of the method 'split' with regular expression.
-     * 
-     * Input: bytes=100-200
-     * Output: [null, 100, 200, null]
-     * 
-     * Input: bytes=-200
-     * Output: [null, null, 200, null]
-     */
-
-if (range == null || range.length == 0)
-    return null;
-
-var array = range.split(/bytes=([0-9]*)-([0-9]*)/);
-var start = parseInt(array[1]);
-var end = parseInt(array[2]);
-var result = {
-    Start: isNaN(start) ? 0 : start,
-    End: isNaN(end) ? (totalLength - 1) : end
-};
-
-if (!isNaN(start) && isNaN(end)) {
-    result.Start = start;
-    result.End = totalLength - 1;
-}
-
-if (isNaN(start) && !isNaN(end)) {
-    result.Start = totalLength - end;
-    result.End = totalLength - 1;
-}
-
-return result;
+function catchErrors(fn) {
+    return function (req, res) {
+      return fn(req, res).catch((err)=> console.error(err));
+    };
 }
 server.listen(8000);
